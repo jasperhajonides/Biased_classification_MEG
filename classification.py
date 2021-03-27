85d3c03d7313ba2a6204ada9b79261db9eb41921
Jasper Hajonides <jasperhajonides@gmail.com>
2021-03-27 16:50:40 +0000

    complete classification script

--- classification.py
+++ classification.py
@@ -39,8 +39,14 @@ processed_loc = projectloc + '/MEG/data/preprocessed'
 bin_array# func
 
 #%% Classifier evidence for participants
+opts = {}
+opts['nsubs'] = 1   
+overwrite = False
+nr_tps = 260
 
-classifier_output = np.zeros((nr_tps,nsubs,4))
+classifier_output = np.zeros((nr_tps,opts['nsubs'],4))
+shift = np.zeros((nr_tps,opts['nsubs']))
+left, right = np.zeros((nr_tps,10,opts['nsubs'])), np.zeros((nr_tps,10,opts['nsubs']))
 
 
 # import stimulus information for participants
@@ -49,25 +55,24 @@ if 'thetas_all' not in  locals():
             [thetas_all, stimulus_nrs, presented_angles, time] = pickle.load(fp)
             
 
-opts = {}
-opts['nsubs'] = 20            
+       
             
-for sb_count in range(0, opts['nsubs']):
+for sb_count in range(19, opts['nsubs']):
     print('computing subject {}'.format(sb_count))
-    dec_fname = '/Users/jasperhajonides/Documents/EXP8_UpdProtec/scores/S20_200hz_LongPreCuePeriod_TEMP_single_tp_time-0.4_to_0.9_LDA_windowsize_30_pca0.9_steps1_stim_all_chans_test_on_all_trials'
+    dec_fname = '/Users/jasperhajonides/Documents/EXP8_UpdProtec/scores/S20_200hz_LongPreCuePeriod_TEMP_single_tp_time-0.4_to_0.9_LDA_windowsize_30_pca0.9_steps1_stim_all_chans_test_on_all'
     if  not(os.path.exists(dec_fname)) or overwrite == True:
     
         # read MEG data
         with open (processed_loc + '/neurophys/MEG_data_200Hz_%s' 
                    %subject_labels[sb_count], 'rb') as fp:
-            [X] = pickle.load(fp)
+            X = pickle.load(fp)
        
         # read behaviour / orientations
         df_read = pd.read_csv(projectloc + '/data/behavioural/all_behavioural.csv')
         df_read = df_read.loc[(df_read['subject'] == sb_count), :]
     
         # include trials
-        inx =   (df_read['stimulus_nr']==2) #& ~np.isnan(df_read['stim1'])  # & reject_outliers(abs(df_read['error']), m=2) & reject_outliers(df_read['RT'], m=3) # & ~np.isnan(df_read['stim2'])  & (abs(df_read['error']/math.pi*90)<math.pi/4) & (df_read['RT']<6) &  #
+        inx =   (df_read['stimulus_nr']<3) #& ~np.isnan(df_read['stim1'])  # & reject_outliers(abs(df_read['error']), m=2) & reject_outliers(df_read['RT'], m=3) # & ~np.isnan(df_read['stim2'])  & (abs(df_read['error']/math.pi*90)<math.pi/4) & (df_read['RT']<6) &  #
         X = X[inx, :, :]
     
         # select & set the target variable
@@ -96,24 +101,32 @@ for sb_count in range(0, opts['nsubs']):
         # load classifier output
         print('preloaded data imported')
         with open (dec_fname, 'rb') as fp:
-            [evidence,time_] = pickle.load(fp)
+            [evidence, time_] = pickle.load(fp)
             
     # read behaviour
     df_read = pd.read_csv(projectloc + '/data/behavioural/all_behavioural.csv')
-    df_read = df_read.loc[(df_read['subject'] == sb_count) ,:]
+    df_read = df_read.loc[(df_read['subject'] == sb_count), :]
     
     # select trials used for classification
-    inx =  (df_read['stimulus_nr'] == 2 )
-    
+    inx =  (df_read['stimulus_nr'] < 3 )
+    # select a subset of trials for analysis
+    inx2 =  (df_read['cue'][inx] == 2 )
+
+    evidence['single_trial_evidence'] = evidence['single_trial_evidence'][inx2]
+
+
     # cross decoding
-    for i, variable in enumerate(['stim1','prev_probe_ang']):
+    for i, variable in enumerate(['stim1', 'prev_probe_ang']):
         # bin continuous variable into discrete classes
-        y, evidence['y'] = bin_array(np.array(df_read[variable][inx]),10)
+        y, evidence['y'] = bin_array(np.array(df_read[variable][inx][inx2]), 10)
         evidence = cos_convolve(evidence)
-        classifier_output[:,sb_count,i] = evidence['cos_convolved']
+        classifier_output[:, sb_count, i] = evidence['cos_convolved']
     
-    
-             
+    # compute shift in tuning curve
+    y_diff = pycircstat.cdiff(df_read['stim1'],df_read['presented'])
+    s, c, w = compute_asymmetry(evidence['single_trial_ev_centered'],
+                                y_diff[inx][inx2], min_deg = 30, max_deg = 60)     
+    shift[:, sb_count], left[:, :, sb_count], right[:, :, sb_count] = s, c, w
         
         
             
--- tools/constr.py
+++ tools/constr.py
@@ -14,4 +14,21 @@ def bin_array(y,nr_bins):
     y = np.array(y)
     bins = np.arange(-math.pi-0.000001,math.pi - (2*math.pi/nr_bins),(2*math.pi/nr_bins))
     y_bin = np.digitize(pycircstat.cdiff(y,0), bins) 
-    return y,y_binr
+    return y,y_bin
+
+
+def matrix_vector_shift(matrix, vector, n_bins):
+
+    """ Shift rows of a matrix by the amount of columns specified
+		in the corresponding cell of the vector.
+
+	e.g. M =0  1  0     V = 0 0 1 2     M_final =   0 1 0
+			0  1  0									 0 1 0
+			1  0  0									 0 1 0
+			0  0  1								  	 0 1 0
+            """
+    row, col = matrix.shape
+    matrix_shift = np.zeros((row, col))
+    for row_id in range(0, row):
+        matrix_shift[row_id, :] = np.roll(matrix[row_id, :], int(np.floor(n_bins/2)-vector[row_id]))
+    return matrix_shift
